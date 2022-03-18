c4c61b2ed5e5e922ac6db41a73a25548f5250e66
firemaples <firemaples@gmail.com>
2022-03-18 14:07:01 +0800

    Upgrade Gradle & Android Gradle plugin, migrate to AndroidX and upgrade target version

--- app/src/main/java/com/firemaples/installreferrertest/MainActivity.java
+++ app/src/main/java/com/firemaples/installreferrertest/MainActivity.java
@@ -1,7 +1,7 @@
 package com.firemaples.installreferrertest;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Html;
 import android.widget.TextView;
 
